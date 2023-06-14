5fcca908bf32fd6cfbb82292024c356835623be1
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-06-14 20:44:21 -0300

    aula20

--- aulas/aula20.py
+++ aulas/aula20.py
@@ -35,6 +35,22 @@
 # A soma A + B = 9'''
 
 
+# def contador(* num):
+#     for valor in num:
+#         print(f'{valor} ', end='')
+#     print('FIM!')
+
+
+# def contador(* num):
+#     tam = len(num)
+#     print(tam)
+#
+#
+# contador(2, 1, 9)
+# contador(8, 0)
+# contador(4, 4, 5, 8, 9)
+
+
 # def soma(* valores): #desempacotamento
 #     s = 0
 #     for num in valores:
@@ -54,3 +70,13 @@ def dobra(lst):
 valores = [6, 3, 9, 1, 0, 2]
 dobra(valores)
 print(valores)
+
+def soma(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}')
+
+
+soma(5, 2)
+soma(2, 9, 4)
