c4f4ef4096aa71a3eeb0302475ab5ba3287edf0c
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-08-02 10:09:09 -0300

    aula115c

--- exercicios/ex115/sistema.py
+++ exercicios/ex115/sistema.py
@@ -1,12 +1,25 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
     resposta = menu(['Ver Pessoa Cadastrada', 'Cadastrar nova Pessoa', 'Sair do Sistema', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        # Opção de listar o conteúdo de um arquivo.
+        lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        # Opção de cadastrar uma pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabeçalho('Saindo do sistema... Até logo!')
         break
     else:
